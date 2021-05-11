b04903852a6b2650cb8bbbcb5e1e254e0a9af62c
GulovAli <planeta...333>
2021-05-12 00:02:50 +0500

    13

--- app/src/main/java/ru/gulov/university/DetailUniverseActivity.java
+++ app/src/main/java/ru/gulov/university/DetailUniverseActivity.java
@@ -1,18 +1,24 @@
+
 package ru.gulov.university;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class DetailUniverseActivity extends AppCompatActivity {
 
     int position = 0;
 
     TextView f1, f2, f3, f4, f5, f6, f7, s1, s2, s3, s4, s5, s6, s7;
-
+    ConstraintLayout fac8, fac9, fac10, fac11;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,48 @@ public class DetailUniverseActivity extends AppCompatActivity {
 
         if(position==1) {
             setContentView(R.layout.activity_detail_universe);
+
+            fac8 = findViewById(R.id.fac8);
+            fac9 = findViewById(R.id.fac9);
+            fac10 = findViewById(R.id.fac10);
+            fac11 = findViewById(R.id.fac11);
+
+            fac8.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri uri = Uri.parse("https://www.uim.ru/"); // missing 'http://' will cause crashed
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            });
+            fac9.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri uri = Uri.parse("https://vk.com/prouim"); // missing 'http://' will cause crashed
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            });
+            fac10.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri uri = Uri.parse("https://krasnodar.ucheba.ru/uz/17686"); // missing 'http://' will cause crashed
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            });
+            fac11.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String uri = String.format(Locale.ENGLISH, "geo:%f,%f", 46.61037580, 39.14418178);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                    startActivity(intent);
+
+                }
+            });
+
+
+
             f1 = findViewById(R.id.fac1);
             f2 = findViewById(R.id.fac2);
             f3 = findViewById(R.id.fac3);
@@ -134,6 +182,10 @@ public class DetailUniverseActivity extends AppCompatActivity {
             });
 
 
+        }
+
+        else if (position==2){
+            setContentView(R.layout.activity_detail_universe2);
         }
 
 
